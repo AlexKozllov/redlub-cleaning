afad7b4806e361192f8abccc5615413d392cbfc0
Alex_Kozzlov <alexandr.kozlov1120@gmail.com>
2021-07-07 02:18:29 +0300

    added slider

--- src/components/howDoWeClean/HowDoWeClean.js
+++ src/components/howDoWeClean/HowDoWeClean.js
@@ -1,10 +1,59 @@
 import React from "react";
-import { HowDoWeStyles } from "./HowDoWeStyles";
-import kitchen from "../../images/kitchen.png";
+import { HowDoWeStyles, RoomSlider, ItemStyles } from "./HowDoWeStyles";
+import kitchenbg from "../../images/kitchenbg.jpg";
+import { useRef } from "react";
+import { useOnScreen } from "../hooks/useOnScreen";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const HowDoWeClean = () => {
+  const ref = useRef();
+
+  const onScreen = useOnScreen(ref, "-470px");
+  console.log("onScreen: ", onScreen);
+
+  const settings = {
+    customPaging: function (i) {
+      return <a>{i}</a>;
+    },
+    arrows: false,
+    dots: true,
+    dotsClass: "slick-dots slick-thumb",
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
-    <HowDoWeStyles>
+    <HowDoWeStyles ref={ref}>
       <div className="howDoWeClean-container">
         <div className="howDoWeClean-wrapper">
           <h2 className="howDoWeClean-header">Как мы убираем</h2>
@@ -13,9 +62,33 @@ const HowDoWeClean = () => {
             приступает к делу. Вам остаётся только оценить результат.
           </p>
         </div>
-        <img className="howDoWeClean-img" src={kitchen} alt="kitchen" />
+        <RoomSlider>
+          <div className="roomBar">asdfsdf</div>
+          <div className="slider-wrapper">
+            <Slider {...settings}>
+              <Item />
+              <Item />
+              <Item />
+              <Item />
+            </Slider>
+          </div>
+          {/* </div> */}
+          {/* <div className="img-wrapper">
+            <img className="howDoWeClean-img" src={kitchenbg} alt="kitchen" />
+          </div> */}
+        </RoomSlider>
       </div>
     </HowDoWeStyles>
+  );
+};
+
+const Item = () => {
+  return (
+    <ItemStyles>
+      <div className="img-wrapper">
+        <img className="howDoWeClean-img" src={kitchenbg} alt="kitchen" />
+      </div>
+    </ItemStyles>
   );
 };
 
--- src/components/howDoWeClean/HowDoWeStyles.js
+++ src/components/howDoWeClean/HowDoWeStyles.js
@@ -3,15 +3,20 @@ import styled from "styled-components";
 const HowDoWeStyles = styled.section`
   width: 100%;
   margin-top: 194px;
+  /* display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: center; */
 
   .howDoWeClean-container {
-    width: 1094px;
     height: 795px;
+    height: 800px;
     margin: 0 auto;
   }
 
   .howDoWeClean-wrapper {
-    width: 100%;
+    width: 1094px;
+    /* width: 100%; */
     height: 113px;
     display: flex;
     flex-direction: row;
@@ -39,10 +44,72 @@ const HowDoWeStyles = styled.section`
     line-height: 140%;
     color: #404040;
   }
-
-  .howDoWeClean-img {
-    margin-top: 52px;
-  }
 `;
 
-export { HowDoWeStyles };
+const ItemStyles = styled.div`
+  /* & .img-wrapper {
+    max-width: 1065px;
+    width: 76%;
+    height: 100%;
+    position: relative;
+    overflow: hidden;
+  } */
+  /* & .howDoWeClean-img {
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    object-fit: cover;
+  } */
+`;
+
+const RoomSlider = styled.div`
+  width: 100%;
+  height: 100%;
+  margin-bottom: 0 auto;
+  margin-top: 52px;
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: center;
+
+  .slider-wrapper {
+    width: 76%;
+  }
+  .myDiv {
+    width: 50px;
+    height: 50px;
+    background: red;
+  }
+  h3 {
+  }
+  .roomBar {
+    width: 26%;
+    height: 100%;
+    background: blue;
+    z-index: 100;
+  }
+  /* .img-wrapper {
+    max-width: 1065px;
+    width: 76%;
+    height: 100%;
+    position: relative;
+    overflow: hidden;
+  }
+  .howDoWeClean-img {
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    object-fit: cover;
+  } */
+ 
+  }
+  
+`;
+
+export { HowDoWeStyles, RoomSlider, ItemStyles };
